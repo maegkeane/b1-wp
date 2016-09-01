81a56e410c3fa16177e376c3d0d2072faf4c42c6
Maeg Keane <maeg.keane@gmail.com>
2016-09-01 15:52:41 -0400

    Wrapped PHP loop in <article> <section> tags
    <header> around h1 too

--- wp-content/themes/bioone-starter-theme/index.php
+++ wp-content/themes/bioone-starter-theme/index.php
@@ -1,10 +1,17 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			
-	<h1><?php the_title(); ?></h1>
-	<?php the_content(); ?>
+<article>
+  <section>
 
-<?php endwhile; endif; ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    	<header>		
+      	<h1><?php the_title(); ?></h1>
+      </header>
+      	<?php the_content(); ?>
+
+    <?php endwhile; endif; ?>
+
+  </section>
+</article>
 
 <?php get_footer(); ?>
