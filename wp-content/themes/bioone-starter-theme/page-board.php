c4676e5c4ef389ef28a20de81e27031536a24172
Maeg Keane <maeg.keane@gmail.com>
2016-09-20 12:20:59 -0400

    Added Board PHP page

--- wp-content/themes/bioone-starter-theme/page-board.php
+++ wp-content/themes/bioone-starter-theme/page-board.php
@@ -8,69 +8,49 @@
 <?php get_header(); ?>
 
 <article>
-
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  	
-    <header>		
-  	    <h1><?php the_title(); ?></h1>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    
+    <header>    
+        <h1><?php the_title(); ?></h1>
     </header>
 
     <?php the_content(); ?>
 
   <?php endwhile; endif; ?>
- 
-  <h3><?php the_field('hq_staff_header'); ?></h3>
 
   <div class="flex-grid">
-
-    <?php if( have_rows( 'bio_grid_hq' ) ) : while ( have_rows( 'bio_grid_hq' ) ) : the_row(); 
-
-      $bio_photo = get_sub_field('bio_photo');
-      $name = get_sub_field('name');
-      $degree = get_sub_field('degree');
-      $title = get_sub_field('title');
-
-    ?>
- 
-    <div class="bio-list-item">
-      <a class="bio-list-item_short-bio-container">
-        <img class="bio-list-item_image" src="<?php echo $bio_photo['url']; ?>" alt="<?php echo $bio_photo['alt'] ?>" /> 
-        <p class="bio-list-item_info">
-            <span class="bio-list-item_name"><?php echo $name; ?><span class="bio-list-item_name"><?php echo $degree; ?> <br />
-          <?php echo $title; ?>
-        </p>
-      </a>
-    </div>
-
-    <?php endwhile; endif; wp_reset_postdata(); ?>
-
-  </div>
-
-  <h3><?php the_field('sales_staff_header'); ?></h3>
-
-  <div class="flex-grid">
+  <?php if (have_rows('bio_grid') ) : while (have_rows ('bio_grid') ) : the_row(); 
     
-    <?php if( have_rows( 'bio_grid_sales' ) ) : while ( have_rows( 'bio_grid_sales' ) ) : the_row(); 
 
       $bio_photo = get_sub_field('bio_photo');
       $name = get_sub_field('name');
-      $title = get_sub_field('title');
+      $position = get_sub_field('position');
+      $affiliation = get_sub_field('affiliation');
+      $degree = get_sub_field('degree');
+      $bio = get_sub_field('bio');
 
     ?>
- 
+
     <div class="bio-list-item">
       <a class="bio-list-item_short-bio-container">
         <img class="bio-list-item_image" src="<?php echo $bio_photo['url']; ?>" alt="<?php echo $bio_photo['alt'] ?>" /> 
         <p class="bio-list-item_info">
-          <?php echo $name; ?> <br />
-          <?php echo $title; ?>
+          <span class="bio-list-item_name">
+            <?php echo $name; ?>
+          </span>
+            </br>
+            <?php echo $affiliation; ?>
         </p>
       </a>
+      <div class="bio-list-item_modal">
+        <span class="close">X</span>
+        <h2><?php echo $name; ?></h2> 
+        <?php echo $bio ?>
+      </div>
     </div>
-
     <?php endwhile; endif; wp_reset_postdata(); ?>
-
-  </div>
+  </div> 
 </article>
+
 
 <?php get_footer(); ?>
