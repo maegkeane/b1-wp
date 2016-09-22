92fb19fbe11353d6d752eea58c28819aeb66fc70
Maeg Keane <maeg.keane@gmail.com>
2016-09-22 13:28:03 -0400

    Updated 404 Page

--- wp-content/themes/bioone-starter-theme/404.php
+++ wp-content/themes/bioone-starter-theme/404.php
@@ -5,10 +5,11 @@
 <h1 class="entry-title"><?php _e( 'Not Found', 'blankslate' ); ?></h1>
 </header>
 <section class="entry-content">
-<p><?php _e( 'Nothing found for the requested page. Try a search instead?', 'blankslate' ); ?></p>
-<?php get_search_form(); ?>
+<p><?php _e( 'Hm. That is strange. Nothing here. Let us know at', 'blankslate' ); ?>
+  <a href="mailto:maeg@arl.org">maeg@arl.org.
+  </a>
+</p>
 </section>
 </article>
 </section>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
