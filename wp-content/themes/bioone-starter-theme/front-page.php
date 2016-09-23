2b40b374fdd51a24d1f821566678738b42a53058
Maeg Keane <maeg.keane@gmail.com>
2016-09-23 17:48:44 -0400

    Added 'slideshow' for testimonials

--- wp-content/themes/bioone-starter-theme/front-page.php
+++ wp-content/themes/bioone-starter-theme/front-page.php
@@ -10,6 +10,34 @@
     <?php endwhile; endif; ?>
 
   </section>
+  <section>
+    
+    <h1>From the Community</h1>
+
+    <div class="cycle-slideshow" 
+    data-cycle-fx="fade" 
+    data-cycle-timeout="20000"
+    data-cycle-slides="> blockquote"
+    >
+
+    <?php if( have_rows( 'testimonials' ) ) : while ( have_rows( 'testimonials' ) ) : the_row(); 
+
+      $blockquote = get_sub_field('blockquote');
+      $name = get_sub_field('name');
+      $title = get_sub_field('title');
+      $institution = get_sub_field('institution');
+
+    ?>
+      <blockquote>
+        <?php echo $blockquote; ?>
+        <p class="blockquote-source">– <?php echo $name; ?> <br /><?php echo $title; ?>, <?php echo $institution; ?></p>
+      </blockquote>
+
+    <?php endwhile; endif; wp_reset_postdata(); ?>
+    
+    </div>
+
+</section>
 </article>
 
 <?php get_footer(); ?>
