ba555955e3516035e97f6378869c9956d334e688
Maeg Keane <maeg.keane@gmail.com>
2016-09-28 11:18:01 -0400

    Added publications page

---
+++ wp-content/themes/bioone-starter-theme/page-publications.php
@@ -0,0 +1,45 @@
+<?php 
+/*
+  Template Name: Publications Page 
+*/
+?> 
+
+
+<?php get_header(); ?>
+
+<article>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    
+    <header>    
+        <h1><?php the_title(); ?></h1>
+    </header>
+
+    <?php the_content(); ?>
+
+  <?php endwhile; endif; ?>
+
+  <?php if( have_rows( 'publication_list' ) ) : while ( have_rows( 'publication_list' ) ) : the_row(); 
+
+    $logo = get_sub_field('logo');
+    $link = get_sub_field('link');
+    $description = get_sub_field('description');
+    $button = get_sub_field('button');
+
+  ?>
+    <a href="<?php $link; ?>">
+      <img class="pub-logo" src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>"/>
+    </a>
+
+  <section>
+    <p><?php $description; ?></p>
+  </section>
+ 
+  <section>  
+    <a class="btn-main" href="<?php $link; ?>"><?php echo $button; ?></a>
+  </section>
+
+  <?php endwhile; endif; wp_reset_postdata(); ?>
+
+</article>
+
+<?php get_footer(); ?>
