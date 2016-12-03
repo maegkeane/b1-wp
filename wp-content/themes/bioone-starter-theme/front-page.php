1e5609294fe7e2fbddb1d788af0e460d79da724c
Maeg Keane <maeg.keane@gmail.com>
2016-12-03 16:29:27 -0500

    Updated front-page.php conference grid. Working! But in the wrong order

--- wp-content/themes/bioone-starter-theme/front-page.php
+++ wp-content/themes/bioone-starter-theme/front-page.php
@@ -100,15 +100,15 @@
   </section>
   <section>
     <h2>Upcoming Conferences</h2>
-
+    <br />
     <?php 
       $the_query = new WP_Query(array( // Define query
         'post_type' => 'conferences',
-        'posts_per_page' => 200, // for conferences, you don't want them to only get 3, you are looking at all of them (say, 200)
+        'posts_per_page' => 200, // For conferences, you don't want them to only get 3, you are looking at all of them (say, 200)
         'orderby' => 'date'
       ));
       
-      $conferenceCalendarArray = array(); //overall grid of conferences
+      $conferenceCalendarArray = array(); // Overall grid of conferences
 
       if ( $the_query->have_posts() ) {
         while ( $the_query->have_posts() ) {
@@ -118,20 +118,23 @@
           // for each post, so into the array we pass another array.
           // So each value in the array is another array with more values.
           $conferenceArray = array(       
-            'conference_name' => get_the_title(),
-            'display_date' => get_sub_field('display_date'),
-            'location' => get_sub_field('location'),
-            'attendees' => get_sub_field('attendees'),
-            'booth' => get_sub_field('booth'),
-            'order_date' => get_sub_field('order_date'),
+            'event_name' => get_the_title(),
+            'display_date' => get_field('display_date'), 
+            'location' => get_field('location'),
+            'attendees' => get_field('attendees'),
+            'booth' => get_field('booth'),
+            'order_date' => get_field('order_date')
           );
-      // array_push adds an item to the end of an existing array
+          
+            // array_push adds an item to the end of an existing array
             array_push($conferenceCalendarArray, $conferenceArray);
           }
-          wp_reset_postdata(); // Restore original post data to prevent unintended functionality
+
+        wp_reset_postdata(); // Restore original post data to prevent unintended functionality
+          
         } else {
-          // no posts found
-      }
+            // No posts found
+        }
 
       function date_compare($a, $b)
       {
@@ -141,24 +144,23 @@
       }    
 
       usort($conferenceCalendarArray, 'date_compare');
-    
-      echo '<div class="card_container">';
-      //this is where we are actually putting things on the page, do $i<count(2); so only three appear
+
+      echo '<div class="card-container">';
+      //This is where we are actually putting things on the page; type $i<count(2), so only 3 appear
       for ($i=0; $i<3; $i++) { // Do another loop to get stuff out of the array
         echo 
-          '<a class="card_calendar">' .
-            $conferenceCalendarArray[$i]['conference_name'] .
-            '<h3>' $date; '</h3>' .
-            '<h2>' $event_name; '</h2>' .
-            '<p>'  $location; '</p>' .
-            '<div class="card_calendar_expand">' . 
-              '<p class="who">Who youll see there: </br>' .
-               $attendees; '</p>' .
-              if (get_sub_field('booth')) :   
-              '<p class="where">' $booth; '</br>' .
-            '</div>' .
-          '</a>';
-        }
+          '<a class="card_calendar">
+            <h3>' . $conferenceCalendarArray[$i]['display_date'] . '</h3>
+            <h2>' . $conferenceCalendarArray[$i]['event_name'] . '</h2>
+            <p>' . $conferenceCalendarArray[$i]['location'] . '</p>
+            <div class="card_calendar_expand">
+              <p class="who">' .
+                $conferenceCalendarArray[$i]['attendees'] .
+              '</p>' .
+              //if (get_sub_field('booth')) : . '<p class="where">' $conferenceCalendarArray[$i]['booth'] . '</br>   
+            '</div>
+          </a>';
+      }
       echo '</div>';
     ?>
 
@@ -223,6 +225,47 @@
               <input type="email" placeholder="Enter your email" name="EMAIL" class="required email" id="mce-EMAIL">
             </div>
   <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                <input type="text" name="b_c96bf0ec63cc75c323952cced_13cf7c0c45" tabindex="-1" value="">
+              </div>
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn-main">
+              </div>
+          </form>
+        </div>
+  <!--End mc_embed_signup-->
+    </div>  
+  </section>
+  <section> 
+    <h1>From the Community</h1>
+
+    <div class="cycle-slideshow" 
+      data-cycle-fx=scrollHorz
+      data-cycle-timeout=0
+      data-cycle-slides="> blockquote"
+      > 
+      <div class="cycle-pager"></div>
+      
+      <?php if( have_rows( 'testimonials' ) ) : while ( have_rows( 'testimonials' ) ) : the_row(); 
+      
+        $blockquote = get_sub_field('blockquote');
+        $name = get_sub_field('name');
+        $title = get_sub_field('title');
+        $institution = get_sub_field('institution');
+      
+      ?>
+      
+        <blockquote>       
+          <?php echo strip_tags($blockquote); ?>
+          <p class="blockquote-source">– <?php echo $name; ?> <br /><?php echo $title; ?>, <?php echo $institution; ?></p>
+        </blockquote>
+      
+      <?php endwhile; endif; wp_reset_postdata(); ?>
+
+    </div>
+  </section>
+</article>
+
+<?php get_footer(); ?>ot remove this or risk form bot signups-->
               <div style="position: absolute; left: -5000px;" aria-hidden="true">
                 <input type="text" name="b_c96bf0ec63cc75c323952cced_13cf7c0c45" tabindex="-1" value="">
               </div>
