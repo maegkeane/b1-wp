929251f1c123bfe6049e29db1e6db816cc06ab4b
Maeg Keane <maeg.keane@gmail.com>
2016-12-07 16:40:44 -0500

    Styling Press Release Posts
    Still need to remove comments.

--- wp-content/themes/bioone-starter-theme/entry-meta.php
+++ wp-content/themes/bioone-starter-theme/entry-meta.php
@@ -1,5 +1,4 @@
 <section class="entry-meta">
-<span class="author vcard"><?php the_author_posts_link(); ?></span>
-<span class="meta-sep"> | </span>
-<span class="entry-date"><?php the_time( get_option('date_format') ); ?></span>
+<span class="meta-sep"></span>
+<h5>Released: <?php the_time( get_option('date_format') ); ?></h5>
 </section>
--- wp-content/themes/bioone-starter-theme/entry.php
+++ wp-content/themes/bioone-starter-theme/entry.php
@@ -1,8 +1,14 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <header>
-<?php if ( is_singular() ) {echo '<h1 class="entry-title">';} else {echo '<h2 class="entry-title">';} ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) {echo '</h1>';} else {echo '</h2>';} ?> <?php edit_post_link(); ?>
+<?php edit_post_link(); ?>
+<h2><?php echo strip_tags(get_field('press_release_header'), '<em>'); ?></h2>
+<?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> 
 <?php if (!is_search()) get_template_part('entry', 'meta'); ?>
 </header>
+
+<?php echo the_field('press_release_description');?>
 <?php get_template_part('entry', (is_archive() || is_search() ? 'summary' : 'content')); ?>
+
 <?php if (!is_search()) get_template_part('entry-footer'); ?>
+
 </article>
