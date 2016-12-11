f637acfc53ab552a42fb8a2fd0abc49fe45bd427
Maeg Keane <maeg.keane@gmail.com>
2016-12-11 16:49:50 -0500

    Uploaded SVGs for social icons

--- wp-content/themes/bioone-starter-theme/footer.php
+++ wp-content/themes/bioone-starter-theme/footer.php
@@ -23,14 +23,25 @@
           <a href="mailto:hello@bioone.org">hello@bioone.org</a>
         </li>
         <li>
-          <a class="image" href="https://twitter.com/BioOneNews" target="_blank">
-            <img src="<?php bloginfo('template_url'); ?>/img/social/twitter.png" alt="twitter" />
+          <a class="social-logo-link" href="https://twitter.com/BioOneNews" target="_blank">
+            <svg class="social-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+              <circle class="st1" cx="15" cy="15" r="14.4"/>
+              <path class="st0" d="M25 10c-0.7 0.3-1.5 0.5-2.3 0.6 0.8-0.5 1.4-1.3 1.7-2.2 -0.8 0.5-1.6 0.8-2.5 1 -0.7-0.8-1.7-1.2-2.9-1.2 -2.2 0-4 1.8-4 4 0 0.3 0 0.6 0.1 0.9C11.9 12.9 9 11.3 7 8.9c-0.3 0.6-0.5 1.3-0.5 2 0 1.4 0.7 2.6 1.8 3.3 -0.6 0-1.3-0.2-1.8-0.5 0 0 0 0 0 0 0 1.9 1.4 3.5 3.2 3.9 -0.3 0.1-0.7 0.1-1 0.1 -0.3 0-0.5 0-0.7-0.1 0.5 1.6 2 2.7 3.7 2.7 -1.4 1.1-3.1 1.7-4.9 1.7 -0.3 0-0.6 0-0.9-0.1 1.8 1.1 3.8 1.8 6.1 1.8 7.3 0 11.3-6 11.3-11.3 0-0.2 0-0.3 0-0.5C23.8 11.5 24.4 10.8 25 10z"/>
+            </svg>
           </a>
-          <a class="image" href="https://www.facebook.com/bioone.org/" target="_blank">
-              <img src="<?php bloginfo('template_url'); ?>/img/social/fb.png" alt="facebook" />
+          <a class="social-logo-link" href="https://www.facebook.com/bioone.org/" target="_blank">
+            <svg class="social-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+              <path class="st0" d="M12.4 24.6v-9H10v-3.2h2.4V9.5c0-2.2 1.4-4.2 4.7-4.2 1.3 0 2.3 0.1 2.3 0.1l-0.1 3c0 0-1 0-2.1 0 -1.2 0-1.4 0.5-1.4 1.4v2.4h3.5l-0.2 3.2h-3.4v9H12.4z"/>
+              <circle class="st1" cx="15" cy="15" r="14.4"/>
+            </svg>
           </a>
-          <a class="image" href="https://www.linkedin.com/company/bioone-" target="_blank">
-            <img src="<?php bloginfo('template_url'); ?>/img/social/linkedin.png" alt="linkedin" />
+          <a class="social-logo-link" href="https://www.linkedin.com/company/bioone-" target="_blank">
+            <svg class="social-logo"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+              <circle class="st1" cx="15" cy="15" r="14.4"/>
+              <path class="st0" d="M8.6 6.3c-1.1 0-2 0.9-2 2 0 1.1 0.9 2 2 2 1.1 0 2-0.9 2-2C10.6 7.2 9.7 6.3 8.6 6.3L8.6 6.3zM8.6 6.3"/>
+              <rect x="6.9" y="11.9" class="st0" width="3.5" height="11.2"/>
+              <path class="st0" d="M19.3 11.6c-1.7 0-2.8 0.9-3.3 1.8h0v-1.5h-3.3v11.2H16v-5.6c0-1.5 0.3-2.9 2.1-2.9 1.8 0 1.8 1.7 1.8 3v5.5h3.5V17C23.4 14 22.8 11.6 19.3 11.6L19.3 11.6zM19.3 11.6"/>
+            </svg>
           </a>
         </li>
       </ul>
