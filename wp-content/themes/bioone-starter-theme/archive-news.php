8143a61e1bbac95023234e871582f5f701eb7f1c
Maeg Keane <maeg.keane@gmail.com>
2016-12-17 11:23:41 -0500

    Updated News Archive code
    - Added my own formatting

--- wp-content/themes/bioone-starter-theme/archive-news.php
+++ wp-content/themes/bioone-starter-theme/archive-news.php
@@ -1,17 +1,34 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php 
-if ( is_day() ) { printf( __( 'Daily Archives: %s', 'blankslate' ), get_the_time(get_option('date_format') ) ); }
-elseif ( is_month() ) { printf( __( 'Monthly Archives: %s', 'blankslate' ), get_the_time('F Y') ); }
-elseif ( is_year() ) { printf( __( 'Yearly Archives: %s', 'blankslate' ), get_the_time('Y') ); }
-else { _e( 'Archives', 'blankslate' ); }
-?></h1>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part('entry'); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part('nav', 'below'); ?>
-</section>
-<?php get_sidebar(); ?>
+<div class="content">
+    <header><h1>News Archive</h1></header>
+    
+    <ol>
+      
+      <?php $the_query = new WP_Query(array( // Define query
+          'post_type' => 'news',
+          'orderby' => 'date'
+        ));
+        
+        if ( $the_query->have_posts() ) {
+            while ( $the_query->have_posts() ) {
+              $the_query->the_post();
+
+            $postCategoryID = wp_get_post_categories($post->ID)[0];
+            $theCategoryObject = get_category($postCategoryID);
+              
+            $categorySlug = $theCategoryObject->slug;
+      ?>
+
+        <li>
+          <h5><?php echo get_field($categorySlug); ?> |
+            <a href="<?php if (get_field('link')) { echo get_field('link'); } else { echo get_the_permalink(); } ?>" target="_blank">
+              <?php if (get_field('press_release_header') && $categorySlug == 'press_release') { echo strip_tags(get_field('press_release_header'), '<em>');} else { echo get_the_title(); } ?>
+            </a>
+          </h5>
+        </li>
+      <?php } wp_reset_postdata(); } else {} ?>
+    
+    </ol>
+  </section>
+</div>
 <?php get_footer(); ?>
