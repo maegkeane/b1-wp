d28c8131d48100cc625fb12882f7403e97edb2f6
Maeg Keane <maeg.keane@gmail.com>
2016-12-19 21:11:15 -0500

    Added ACF to Contact Us

--- wp-content/themes/bioone-starter-theme/page-contact.php
+++ wp-content/themes/bioone-starter-theme/page-contact.php
@@ -12,6 +12,15 @@
         <?php the_content(); ?>
     <?php endwhile; endif; ?>
   </section>
+  <section>
+    <?php if (have_rows('logos_for_press') ) : while (have_rows ('logos_for_press') ) : the_row(); ?>
+    <a href="<?php get_sub_field('logo_link'); ?>"><img class="press-logos" src="<?php echo get_sub_field('logo_image')['url']; ?>" alt="<?php echo get_sub_field('logo_image')['alt'] ?>" /></a>
+    <p><a href="<?php get_sub_field('logo_link'); ?>"><?php echo get_sub_field('logo_name'); ?></a></p>
+    <?php endwhile; endif; wp_reset_postdata(); ?>
+  </section>
+  <section>
+      <?php the_field('promotional_text'); ?>
+  </section>  
 </div>
 
 <?php get_footer(); ?>
