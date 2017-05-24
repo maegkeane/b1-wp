1db678636f307244fa32a75749e21f53f30c3d53
Maeg Keane <maeg.keane@gmail.com>
2017-05-24 15:43:18 -0400

    Sitemap update

--- wp-content/themes/bioone-starter-theme/404.php
+++ wp-content/themes/bioone-starter-theme/404.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Not Found', 'blankslate' ); ?></h1>
-</header>
-<section class="entry-content">
-<p><?php _e( 'Hm. That is strange. Nothing here. Let us know at', 'blankslate' ); ?>
-  <a href="mailto:maeg@arl.org">maeg@arl.org.
-  </a>
-</p>
-</section>
-</article>
-</section>
+<div class="content">
+  <section id="content" role="main">
+    <article id="post-0" class="post not-found">
+      <header class="header">
+        <h1 class="entry-title"><?php _e( 'Not Found', 'blankslate' ); ?></h1>
+      </header>
+      <section class="entry-content">
+        <p><?php _e( 'Hm. That is strange. Nothing here. Let us know at', 'blankslate' ); ?>
+        <a href="mailto:maeg@arl.org">maeg@arl.org.
+        </a>
+        </p>
+      </section>
+    </article>
+  </section>
+</div>
 <?php get_footer(); ?>
