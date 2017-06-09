bcad73234386b16197186c91555fab8a1a50ae4d
Maeg Keane <maeg.keane@gmail.com>
2017-06-09 11:51:44 -0400

    Fixing news grid

--- wp-content/themes/bioone-starter-theme/page-news.php
+++ wp-content/themes/bioone-starter-theme/page-news.php
@@ -16,50 +16,50 @@
   <section>
     <h2><?php the_field('news_title'); ?></h2>
     <div class="card-container">
-      
+
       <?php   
-        $the_query = new WP_Query(array( // Define query
-          'post_type' => 'news',
-          'posts_per_page' => 6,
-          'orderby' => 'date'
-        ));
-        
-        if ( $the_query->have_posts() ) {
-          echo '<div class="card-container">';
-            while ( $the_query->have_posts() ) {
-              $the_query->the_post();
+          $the_query = new WP_Query(array( // Define query
+            'post_type' => 'news',
+            'posts_per_page' => 6,
+            'orderby' => 'date'
+          ));
+          
+          if ( $the_query->have_posts() ) {
+            echo '<div class="card-container">';
+              while ( $the_query->have_posts() ) {
+                $the_query->the_post();
+                
+                $postCategoryID = wp_get_post_categories($post->ID)[0];
+                $theCategoryObject = get_category($postCategoryID);
+                
+                $categorySlug = $theCategoryObject->slug;
+        ?>
+
+          <a class="card <?php echo $categorySlug; ?>" <?php if (get_field('annual_report_bg_image') && $categorySlug === 'annual_report') { ?>
+            style="background-image: url(<?php echo get_field('annual_report_bg_image')['url']; ?>);"; 
+            <?php } ?>
+            href="<?php if (get_field('link')) { echo get_field('link'); } else { echo get_the_permalink(); } ?>" target="_blank">
+
+            <div class="outline">
+              <h3 class="h3-green"><?php echo get_field($categorySlug); ?></h3>
+              <h2><?php if (get_field('press_release_header') && $categorySlug == 'press_release') { echo strip_tags(get_field('press_release_header'), '<em>');} else { echo get_the_title(); } ?></h2>
+              <?php if (get_field('issue_icon')) : ?>
+                <img src="<?php echo get_field('issue_icon')['url']; ?>" alt="<?php echo get_field('issue_icon')['alt']; ?>" />
+              <?php endif;?> 
               
-              $postCategoryID = wp_get_post_categories($post->ID)[0];
-              $theCategoryObject = get_category($postCategoryID);
-              
-              $categorySlug = $theCategoryObject->slug;
-      ?>
+              <p><?php echo get_field('pdf_note'); ?></p>
+            </div>
+          </a>
 
-        <a class="card <?php echo $categorySlug; ?>" <?php if (get_field('annual_report_bg_image') && $categorySlug === 'annual_report') { ?>
-          style="background-image: url(<?php echo get_field('annual_report_bg_image')['url']; ?>);"; 
-          <?php } ?>
-          href="<?php if (get_field('link')) { echo get_field('link'); } else { echo get_the_permalink(); } ?>" target="_blank">
-
-          <div class="outline">
-            <h3><?php echo get_field($categorySlug); ?></h3>
-            <h2><?php if (get_field('press_release_header') && $categorySlug == 'press_release') { echo strip_tags(get_field('press_release_header'), '<em>');} else { echo get_the_title(); } ?></h2>
-            <?php if (get_field('issue_icon')) : ?>
-              <img src="<?php echo get_field('issue_icon')['url']; ?>" alt="<?php echo get_field('issue_icon')['alt']; ?>" />
-            <?php endif; ?> 
-            
-            <p><?php echo get_field('pdf_note'); ?></p>
-          </div>
-        </a>
-
-      <?php }
-        wp_reset_postdata(); // Restore original post data to prevent unintended functionality          
+        <?php }
+          wp_reset_postdata(); // Restore original post data to prevent unintended functionality          
+          
+          } else {
+              // No posts found
+          }
+        ?>      
         
-        } else {
-            // No posts found
-        }
-      ?>
-      
-    <a class="btn-main" href="http://devmaeg.com/bioone-wp/news" target="_blank">News Archive</a>
+        <a class="btn-main" href="news-archive/" target="_blank">News Archive</a>
   </section>
   <a id="calendar"><hr></a>
   <section>
