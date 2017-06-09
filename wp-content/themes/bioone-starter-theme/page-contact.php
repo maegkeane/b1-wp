86032ca77b2e926f3d46b94d114b3a1c61372532
Maeg Keane <maeg.keane@gmail.com>
2017-06-09 11:52:21 -0400

    Hyperlinks on logos on Contact page, fixed

--- wp-content/themes/bioone-starter-theme/page-contact.php
+++ wp-content/themes/bioone-starter-theme/page-contact.php
@@ -13,9 +13,21 @@
     <?php endwhile; endif; ?>
   </section>
   <section>
-    <?php if (have_rows('logos_for_press') ) : while (have_rows ('logos_for_press') ) : the_row(); ?>
-    <a href="<?php get_sub_field('logo_link'); ?>"><img class="press-logos" src="<?php echo get_sub_field('logo_image')['url']; ?>" alt="<?php echo get_sub_field('logo_image')['alt'] ?>" /></a>
-    <p><a href="<?php get_sub_field('logo_link'); ?>"><?php echo get_sub_field('logo_name'); ?></a></p>
+
+     <?php if( have_rows( 'logos_for_press' ) ) : while ( have_rows( 'logos_for_press' ) ) : the_row(); 
+
+      $image = get_sub_field('logo_image');
+      $link = get_sub_field('logo_link');
+      $logo_name = get_sub_field('logo_name');
+
+    ?>
+
+      <a href="<?php echo $link ;?>">
+        <img class="press-logos" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+      </a>
+      <p>
+        <a href="<?php echo $link; ?>"><?php echo $logo_name; ?></a>
+      </p>
     <?php endwhile; endif; wp_reset_postdata(); ?>
   </section>
   <section>
